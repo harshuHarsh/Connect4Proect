485318270442dccea18bf5faa900e65a8c512102
harshuHarsh <50937875+harshuHarsh@users.noreply.github.com>
2020-04-29 19:44:23 -0400

    Add files via upload

--- CheckWin.java
+++ CheckWin.java
@@ -1,6 +1,415 @@
 package Connect4;
 
-public class CheckWin 
+public class CheckWin extends Board
 {
+	public static boolean flag=true;
+	
+	
+	 public static boolean CheckRedHorizontal(){
+	       
+	        int counter = 0;
+	        while(flag)
+	        {
+
+	           
+	            for(int w = 0; rows > w; w += 1)
+	            {
+	                for(int h = 0; columns > h; h += 1)
+	                {
+	                    if(grid[w][h] == 'R')
+	                    { 
+	                        counter += 1;
+	                    }
+	                    
+	                    else
+	                    {
+	                        counter = 0; 
+	                    }
+	                    if(counter >= 4)
+	                    {
+	                        System.out.println("Player 1 wins"); 
+	                        flag = false;
+	                    }
+	                }
+	            }
+	            break;
+	        }
+	        return flag;
+	    }
+
+	    public static boolean CheckRedVertical(){
+	       
+	        boolean flag = true;
+
+	        int counter = 0;
+	        while(flag)
+	        {
+
+	            for(int h = 0; columns > h; h += 1)
+	            {
+	                for(int w = 0; rows > w; w += 1)
+	                {
+	                    if(grid[w][h] == 'R')
+	                    { 
+	                        counter += 1;
+	                    }
+	                    else
+	                    {
+	                        counter = 0; 
+	                    }
+	                    if(counter >= 4)
+	                    {
+	                        System.out.println("Player 1 wins"); 
+	                        flag = false;
+	                    }
+	                }
+	            }
+	            break;
+	        }
+	        return flag;
+	    }
+
+	    public static boolean CheckYellowVertical()
+	    {
+	       
+	        boolean flag = true;
+
+	       
+	        int counter = 0;
+	        while(flag){
+
+	            
+	            for(int h = 0; columns > h; h += 1){
+	                for(int w = 0; rows > w; w += 1){
+	                    if(grid[w][h] == 'Y'){
+	                        counter += 1;
+	                    }else{
+	                        counter = 0; 
+	                    }
+	                    if(counter >= 4){
+	                        System.out.println("Player 2 wins"); 
+	                        flag = false;
+	                    }
+	                }
+	            }
+	            break;
+	        }
+	        return flag;
+	    }
+
+	    public static boolean CheckYellowHorizontal(){
+	       
+	        boolean flag = true;
+
+	        
+	        int counter = 0;
+	        while(flag){
+
+	            
+	            for(int w = 0; rows > w; w += 1)
+	            {
+	                for(int h = 0; columns > h; h += 1)
+	                {
+	                    if(grid[w][h] == 'Y')
+	                    {
+	                        counter += 1;
+	                    }
+	                    else
+	                    {
+	                        counter = 0; 
+	                    }
+	                    if(counter >= 4)
+	                    {
+	                        System.out.println("Player 2 wins");
+	                        flag = false;
+	                    }
+	                }
+	            }
+	            break;
+	        }
+	        return flag;
+	    }
+
+	    public static boolean CheckRedDiagonalForward(){
+	       
+	        boolean flag = true;
+
+	      
+	        int counter = 0;
+
+	      
+	        Boolean check = false;
+
+	      
+	        int checkColumn = 1;
+	        int checkRow = 1;
+
+	        while(flag)
+		{ 
+	            for(int w = 0; rows > w; w += 1)
+		    {
+	                for(int h = 0; columns > h; h += 1)
+			{
+	                    if(grid[w][h] == 'R')
+			    { 
+	                        counter += 1;
+	                        check = true;
+	                        while(check)
+				{
+	                            if(checkColumn + w <= rows - 1&& checkRow + h <= columns - 1)
+				    {
+	                                if(grid[w + checkColumn][h + checkRow] == 'R')
+					{ 
+	                                    counter += 1;
+	                                }
+	                            }
+
+	                            //adds 1 to checkers
+	                            checkColumn += 1;
+	                            checkRow += 1;
+
+	                            if(checkColumn == rows -1 || checkRow == columns -1)
+				    { 
+	                                check = false;
+	                                break;
+	                            }
+
+	                            if(counter >= 4){
+	                                System.out.println("Player 1 wins"); 
+	                                check = false;
+	                                flag = false;
+	                                break;
+	                            }
+	                        }
+	                    }
+	                    if(counter >= 4){
+	                        flag = false;
+	                        break;
+	                    }
+
+	                    //resets counter and checkers
+	                    counter = 0;
+	                    checkColumn = 1;
+	                    checkRow = 1;
+	                }
+	            }
+	            break;
+	        }
+	        return flag;
+	    }
+
+	    public static boolean CheckYellowDiagonalForward(){
+	    
+	        boolean flag = true;
+
+	      
+	        int counter = 0;
+
+	       
+	        Boolean check = false;
+
+	       
+	        int checkColumn = 1;
+	        int checkRow = 1;
+
+	        while(flag)
+	        { 
+	            for(int w = 0; rows > w; w += 1)
+	            {
+	                for(int h = 0; columns > h; h += 1){
+	                    if(grid[w][h] == 'Y'){ 
+	                        counter += 1;
+	                        check = true;
+	                        while(check){ 
+	                            if(checkColumn + w <= rows - 1&& checkRow + h <= columns - 1){
+	                                if(grid[w + checkColumn][h + checkRow] == 'Y'){ 
+	                                    counter += 1;
+	                                }
+	                            }
+
+	                          
+	                            checkColumn += 1;
+	                            checkRow += 1;
+
+	                            if(checkColumn == rows -1 || checkRow == columns -1){ 
+	                                check = false;
+	                                break;
+	                            }
+
+	                            if(counter >= 4){
+	                                System.out.println("Player 2 wins"); 
+	                                check = false;
+	                                flag = false;
+	                                break;
+	                            }
+	                        }
+	                    }
+	                    if(counter >= 4){
+	                        flag = false;
+	                        break;
+	                    }
+
+	                    //resets counter and checkers
+	                    counter = 0;
+	                    checkColumn = 1;
+	                    checkRow = 1;
+	                }
+	            }
+	            break;
+	        }
+	        return flag;
+	    }
+
+	    public static boolean CheckRedDiagonalReverse(){
+	        //flag
+	        boolean flag = true;
+
+	        //counter
+	        int counter = 0;
+
+	        //check boolean
+	        Boolean check = false;
+
+	        //checkers
+	        int checkColumn = 1;
+	        int checkRow = 1;
+
+	        while(flag){
+ 
+	            for(int w = 0; rows > w; w += 1){
+	                for(int h = 0; columns > h; h += 1){
+	                    if(grid[w][h] == 'R'){ 
+	                        counter += 1;
+	                        check = true;
+	                        while(check){ 
+	                            if(w - checkColumn >= 0 && h - checkRow >= 0){
+	                                if(grid[w - checkColumn][h - checkRow] == 'R'){
+	                                    counter += 1; 
+	                                }
+	                            }
+
+	                            //adds 1 to checkers
+	                            checkColumn += 1;
+	                            checkRow += 1;
+
+	                            if(checkColumn == 0 || checkRow == columns -1){ 
+	                                check = false;
+	                                break;
+	                            }
+
+	                            if(counter >= 4){
+	                                System.out.println("Player 1 wins"); 
+	                                check = false;
+	                                flag = false;
+	                                break;
+	                            }
+	                        }
+	                    }
+	                    if(counter >= 4){
+	                        flag = false;
+	                        break;
+	                    }
+
+	                   
+	                    counter = 0;
+	                    checkColumn = 1;
+	                    checkRow = 1;
+	                }
+	            }
+	            break;
+	        }
+	        return flag;
+	    }
+
+	    public static boolean CheckYellowDiagonalReverse(){
+	     
+	        boolean flag = true;
+
+	        int counter = 0;
+
+	       
+	        Boolean check = false;
+
+	        int checkColumn = 1;
+	        int checkRow = 1;
+
+	        while(flag){
+
+	           
+	            for(int w = 0; rows > w; w += 1){
+	                for(int h = 0; columns > h; h += 1){
+	                    if(grid[w][h] == 'Y')
+	                    { 
+	                        counter += 1;
+	                        check = true;
+	                        while(check)
+	                        {
+	                            if(w - checkColumn >= 0 && h - checkRow >= 0)
+	                            {
+	                                if(grid[w - checkColumn][h - checkRow] == 'Y')
+	                                {
+	                                    counter += 1; //if O is found, add 1 to counter
+	                                }
+	                            }
+
+	                            
+	                            checkColumn += 1;
+	                            checkRow += 1;
+
+	                            if(checkColumn == 0 || checkRow == columns -1)
+	                            { 
+	                                check = false;
+	                                break;
+	                            }
+
+	                            if(counter >= 4)
+	                            {
+	                                System.out.println("Player 2 wins"); 
+	                                check = false;
+	                                flag = false;
+	                                break;
+	                            }
+	                        }
+	                    }
+	                    if(counter >= 4){
+	                        flag = false;
+	                        break;
+	                    }
+
+	                   
+	                    counter = 0;
+	                    checkColumn = 1;
+	                    checkRow = 1;
+	                }
+	            }
+	            break;
+	        }
+	        return flag;
+	    }
+
+	    public boolean CheckX(){
+	       
+	        boolean flag = true;
+
+	        
+	        if(!CheckRedVertical() || !CheckRedHorizontal()|| !CheckRedDiagonalReverse()|| !CheckRedDiagonalForward())
+	        {
+	            flag = false;
+	        }
+	        return flag;
+	    }
+
+	    public  boolean CheckO(){
+	
+	    	
+	        boolean flag = true;
+
+	   
+	        if(!CheckYellowVertical() || !CheckYellowHorizontal() || !CheckYellowDiagonalReverse() || !CheckYellowDiagonalForward()){
+	            flag = false;
+	        }
+	        return flag;
+	    }
+	
 
 }
