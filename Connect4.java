a651fa95547244be80f9246c77cec000ab240bc7
harshuHarsh <50937875+harshuHarsh@users.noreply.github.com>
2020-05-01 22:38:54 -0400

    Add files via upload

--- Connect4.java
+++ Connect4.java
@@ -1,31 +1,108 @@
-package Connect4;
-
-import java.util.Scanner;
-
+package Game;
 public class Connect4
 {
    
-    private boolean win=true; 
+    
     public static void main(String[] args)
     {
     	System.out.println("Select one from 1 - 6 to drop your coin"+"\n");
-    	
     	Board.Creategrid();
     	Board.printgrid();
         System.out.println("Please Choose one color between Red and yellow-Type in R or Y: ");
-        char c =Coin.returnCoin();
-        while(true)
+        char c = Coin.returnCoin();
+        
+        while(CheckWin.flag==true)
         {
-	        Board.printgrid();
-	        Dropcoin.dropRed();
-	        Dropcoin.dropYellow();
-	        Board.printgrid();
+        	
+	        if(c=='Y')
+	        {
+	        	
+	        	if(Dropcoin.i%2==1)
+	        	{
+		        	 Dropcoin.dropYellow();
+		        	 Board.printgrid();
+		        	 if(!CheckWin.CheckX())
+			 	        {
+			 	        	CheckWin.flag = false; 
+			                 break; 
+			             }
+			 	        if(!CheckWin.CheckO())
+			 	        {
+			 	        	CheckWin.flag = false; 
+			                 break; 
+			             }
+		 	        
+	            }
+	        	if(Dropcoin.i%2==0)
+		        {
+	        		 Dropcoin.dropRed();		        		
+		        	 Board.printgrid();
+		        	 if(!CheckWin.CheckX())
+			 	        {
+			 	        	CheckWin.flag = false; 
+			                 break; 
+			             }
+			 	        if(!CheckWin.CheckO())
+			 	        {
+			 	        	CheckWin.flag = false; 
+			                 break; 
+			             }
+		 	        
+			        	
+		        }
+	        	
+	 	        
+	        	
+	        }
+	      
+	        if(c=='R')
+	        {
+	        	
+	        	if(Dropcoin.i%2==1)
+	        	{
+		        	 Dropcoin.dropRed(); 
+		        	 Board.printgrid();
+		        	 if(!CheckWin.CheckX())
+			 	        {
+			 	        	CheckWin.flag = false; 
+			                 break; 
+			             }
+			 	        if(!CheckWin.CheckO())
+			 	        {
+			 	        	CheckWin.flag = false; 
+			                 break; 
+			             }
+		 	        
+	        	
+	        	}
+	        	if(Dropcoin.i%2==0)
+		        {
+	        		
+	        		 Dropcoin.dropYellow();
+		        	 Board.printgrid();
+		        	 if(!CheckWin.CheckX())
+			 	        {
+			 	        	CheckWin.flag = false; 
+			                 break; 
+			             }
+			 	        if(!CheckWin.CheckO())
+			 	        {
+			 	        	CheckWin.flag = false; 
+			                 break; 
+			             }
+		 	        
+			        	
+		        }
+	        	
+	        	
+	        }
+	       
         }
-        
-        
     }
-   
-   
-	
-	
 }
+        
+    
+   
+   
+	
+	
